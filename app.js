c598a5015ef02e3af993abee9371dc89bfb34e35
Lucie Musilová <musil.lucie@seznam.cz>
2021-05-15 14:13:56 +0200

    rozpracovane

--- app.js
+++ app.js
@@ -17,3 +17,54 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
+
+//proměnné
+let seznamReceptu = document.getElementById('recepty');
+
+//zobrazení receptů
+zobrazRecepty();
+
+//zobrazení receptů
+function zobrazRecepty() {
+  for (let i = 0; i < recepty.length; i++) {
+    let recept = document.createElement('div');
+    recept.classList.add('recept');
+
+    let obrazek = document.createElement('div');
+    obrazek.classList.add('recept-obrazek');
+
+    let foto = document.createElement('img');
+
+    let info = document.createElement('div');
+    info.classList.add('recept-info');
+
+    let nazev = document.createElement('h3');
+
+    obrazek.appendChild(foto);
+    info.appendChild(nazev);
+    recept.appendChild(obrazek);
+    recept.appendChild(info);
+    seznamReceptu.appendChild(recept);
+
+    foto.src = recepty[i].img;
+    nazev.innerText = recepty[i].nadpis;
+   } 
+}
+
+//hledání
+
+window.addEventListener("load", vyhledej);
+
+function vyhledej () {
+  // (B) ATTACH KEY UP LISTENER TO SEARCH BOX
+  document.getElementById("hledat").addEventListener("keyup", function(){
+    // (C) GET THE SEARCH TERM
+    let hledej = this.value.toLowerCase();
+    
+    // (E) LOOP THROUGH LIST ITELS - ONLY SHOW ITEMS THAT MATCH SEARCH
+    for (let i = 0; i < recepty.length; i++)  {
+      let item = recepty[i].nadpis.toLowerCase();
+      if (item.indexOf(hledej) == -1) {nazev.style.color = "red"};
+    };
+  });
+};
